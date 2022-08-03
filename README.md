df6945eba2a6d85b983c81d30a9b854ca352c1d9
ariapn <ariapn@gmail.com>
2022-08-02 22:11:02 -0500

    fin550 fall2022

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # packer-rstudio-ami
-Create FIN510-RStudio AMI using packer:
+# ami-00d3a5d3fd461c559
+Create FIN550-RStudio AMI using packer:
 
 - Install [packer](https://learn.hashicorp.com/tutorials/packer/getting-started-install)
 - Edit rstudio.json as needed
-- `AWS_PROFILE=default packer build rstudio.json`
+- `AWS_PROFILE=uiuc-msba-instructor packer build rstudio-msba.json`
 - `aws --profile profile ec2 run-instances --image-id ami-xxxxxxxx --instance-type t2.micro --security-group-ids sg-xxxx --iam-instance-profile Name=iam-instance-profile --key-name MyKeyPair`
 - `aws --profile profile ec2 modify-image-attribute --image-id ami-xxx --launch-permission "Add=[{UserId=$AccountID}]"`
--- install-debs.sh
+++ install-debs.sh
@@ -3,21 +3,26 @@
 set -e
 
 sudo apt-get purge docker.io -y
-sudo apt-get update
+sudo apt-get update -qq
 sudo apt-get dist-upgrade -y
-sudo apt-get install -y software-properties-common
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
+sudo apt install --no-install-recommends software-properties-common dirmngr
+# add the signing key (by Michael Rutter) for these repos
+# To verify key, run gpg --show-keys /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc 
+# Fingerprint: E298A3A825C0D65DFD57CBB651716619E084DAB9
+wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+# add the R 4.0 repo from CRAN -- adjust 'focal' to 'groovy' or 'bionic' as needed
+sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
 sudo apt-get update
 sudo apt-get install -y r-base r-base-dev gdebi-core libcurl4-openssl-dev libxml2-dev libssl-dev
+sudo apt-get install -y lib32gcc-s1 lib32stdc++6 libc6-i386 libclang-14-dev libclang-common-14-dev libclang-dev libclang1-14 libgc1 libllvm14 libobjc-11-dev libobjc4 libpq5
 #sudo apt-get install -y r-cran-boot r-cran-gbm r-cran-ggplot2 r-cran-glmnet r-cran-knitr r-cran-maps \
 #    r-cran-mapproj r-cran-mass r-cran-plm r-cran-randomforest r-cran-rpart r-cran-tidyr r-cran-tidyverse
 chmod a+x /home/ubuntu/install-rpackages.sh
 sudo /home/ubuntu/install-rpackages.sh
 sudo useradd -m -p sayzF.CuJmED6 rstudio
-wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-2021.09.2-382-amd64.deb
-sudo gdebi -n rstudio-server-2021.09.2-382-amd64.deb
-rm -f rstudio-server-2021.09.2-382-amd64.deb
+wget https://download2.rstudio.org/server/jammy/amd64/rstudio-server-2022.07.1-554-amd64.deb
+sudo gdebi -n rstudio-server-2022.07.1-554-amd64.deb
+rm -f rstudio-server-2022.07.1-554-amd64.deb
 sudo tee -a /etc/rstudio/rserver.conf  > /dev/null << EOL
 www-port=80
 EOL
